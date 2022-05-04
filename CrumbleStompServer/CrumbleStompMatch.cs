fd26039eca2d8178d8ea000143582cf4f5ac1ab3
Marc Zaku <mzaku@hotmail.de>
2022-05-04 13:38:44 +0200

    Add CrumbleStomp Matchmaking Server Logic

---
+++ CrumbleStompServer/CrumbleStompMatch.cs
@@ -0,0 +1,14 @@
+using System.Net.Sockets;
+
+namespace CrumbleStompServer;
+
+public class CrumbleStompMatch
+{
+    public TcpClient red;
+    public TcpClient blue;
+
+    public void Start()
+    {
+        
+    }
+}
--- CrumbleStompServer/Program.cs
+++ CrumbleStompServer/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace CrumbleStompServer;
+
+public static class Program
+{
+    public static void Main()
+    {
+        // start listening to new connections on the given socket
+        var tcpListener = new TcpListener(IPAddress.Any, 12244);
+        tcpListener.Start();
+
+        Console.WriteLine("Server listening on: "+tcpListener.LocalEndpoint);
+
+        CrumbleStompMatch match = null;
+
+        while (true)
+        {
+            Console.WriteLine("Waiting for connection...");
+            // wait for a client to establish a connection and return that connection
+            var tcpClient = tcpListener.AcceptTcpClient();
+
+            Console.WriteLine($"Client {tcpClient.Client.RemoteEndPoint} connected!");
+    
+            if (match == null)
+            {
+                Console.WriteLine("Starting new Match for Player. Waiting for Second Player.");
+                match = new CrumbleStompMatch
+                {
+                    red = tcpClient
+                };
+            }
+            else
+            {
+                Console.WriteLine("Assigning Player to existing Match. Starting Match.");
+                match.blue = tcpClient;
+                new Thread(match.Start).Start();
+                match = null;
+            }
+        }
+    }
+}
